34cd0f935b0a29839fafcd1a34e18b7efe57b54b
mmgreiner <mmgreiner@bluewin.ch>
2016-04-05 16:05:25 +0200

    now animation is randomized

--- Ex2.2/_current solution/scripts/animation.js
+++ Ex2.2/_current solution/scripts/animation.js
@@ -12,19 +12,29 @@ Adopted from: http://justinaguilar.com/animations/
 
 "use strict";
 
+function RandomSlide()
+{
+    // get a random number to determine how to animate
+    var num = Math.floor(Math.random() * 4 + 1);
+    return "slide" + num;    
+}
+
 $(document).ready(function() {
     $(".animate").each(function() {
         var elem = $(this).get()[0];
         console.log("elem " + elem.nodeName + " " + elem.id); 
     });
+    
+    
     $(window).scroll(function() {
         $(".animate").each(function() {
             var imagePos = $(this).offset().top;
             var topOfWindow = $(window).scrollTop();
             var height = $(window).innerHeight();
-            console.log("hit scrolling " + imagePos + " " + topOfWindow + " " + height);
+            // console.log("hit scrolling " + imagePos + " " + topOfWindow + " " + height);
             if (imagePos < topOfWindow+height) {
-                $(this).addClass("slide");
+                var slide = RandomSlide();
+                $(this).addClass(slide);
             }
         })
     });
