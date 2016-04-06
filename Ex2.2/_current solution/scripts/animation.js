89c23c4fded0875d0d773c7bd32ddbd071602e45
mmgreiner <mmgreiner@bluewin.ch>
2016-04-06 17:17:38 +0200

    also works buttom up scroll

--- Ex2.2/_current solution/scripts/animation.js
+++ Ex2.2/_current solution/scripts/animation.js
@@ -12,29 +12,40 @@ Adopted from: http://justinaguilar.com/animations/
 
 "use strict";
 
-function RandomSlide()
-{
+function RandomSlide() {
     // get a random number to determine how to animate
     var num = Math.floor(Math.random() * 4 + 1);
-    return "slide" + num;    
+    return "slide" + num;
+}
+
+function AlreadySlided(node) {
+    return $(node).hasClass("slide1") || $(node).hasClass("slide2") ||
+        node.hasClass("slide3") || node.hasClass("slide4");
 }
 
 $(document).ready(function() {
-    $(".animate").each(function() {
-        var elem = $(this).get()[0];
-        console.log("elem " + elem.nodeName + " " + elem.id); 
-    });
-    
-    
+    // on the first page there is no scroll
+    if ($(window).scrollTop() == 0) {
+        $("#home .animate").addClass(RandomSlide());
+        $("#about .animate").addClass(RandomSlide());
+    }
+
     $(window).scroll(function() {
+        console.log("scrolltop " + $(window).scrollTop());
         $(".animate").each(function() {
-            var imagePos = $(this).offset().top;
-            var topOfWindow = $(window).scrollTop();
-            var height = $(window).innerHeight();
-            // console.log("hit scrolling " + imagePos + " " + topOfWindow + " " + height);
-            if (imagePos < topOfWindow+height) {
-                var slide = RandomSlide();
-                $(this).addClass(slide);
+            // if it already contains the class, skips("slide1"));
+            if (!AlreadySlided($(this))) {
+                
+                var imagePos = $(this).offset().top;    
+                var topOfWindow = $(window).scrollTop();
+                var height = $(window).innerHeight();
+                
+                // console.log("hit scrolling " + this.localName + " " + imagePos + " < " + topOfWindow + " " + height);
+                if (topOfWindow < imagePos && imagePos < topOfWindow + height) {
+                    var slide = RandomSlide();
+                    // console.log("sliding " + this.localName);
+                    $(this).addClass(slide);
+                }
             }
         })
     });
